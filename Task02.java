37f261b495d3cc047066a32a45ee3747293a4048
SongWooJo <90338319+SongWooJo@users.noreply.github.com>
2024-10-11 22:57:11 +0900

    Delete Task02.java

--- Task02.java
+++
@@ -1,9 +0,0 @@
-package homework01;
-
-public class Task02 {
-	public static void main(String[] args) {
-		for(int i=0, j=10; i<=5 && j<=10; i++,j--) {
-			System.out.println("i="+ i+"j="+j);
-		}
-	}
-}
