81a24ceea6859ee8d33a48d5b879d2005f5abafe
SongWooJo <90338319+SongWooJo@users.noreply.github.com>
2024-12-07 19:52:56 +0900

    Add files via upload

---
+++ Week14_1/Week14_1/WaitNotifyExample.java
@@ -0,0 +1,16 @@
+package Week14_1;
+
+public class WaitNotifyExample {
+
+	public static void main(String[] args) {
+		WorkObject workObject = new WorkObject();
+		
+		ThreadA threadA = new ThreadA(workObject);
+		ThreadB threadB = new ThreadB(workObject);
+		
+		threadA.start();
+		threadB.start();
+
+	}
+
+}
---
+++ Week14_1/Week14_3/ExecutorServiceExample.java
@@ -0,0 +1,15 @@
+package Week14_3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		
+		executorService.shutdownNow();
+
+	}
+
+}
